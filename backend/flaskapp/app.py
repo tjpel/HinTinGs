d80bb575322054b785fa945c42a26696bd2d5414
Travis Smalling <main@smalling.dev>
2023-06-19 21:56:30 -0500

    more cleaned up routing, and framework for RESTful Api
    I'm working through some guides on using Flask Restful APIs, so good news bad news, might take me a bit longer but I think this could be permanent solution

--- backend/flaskapp/app.py
+++ backend/flaskapp/app.py
@@ -1,8 +1,21 @@
-from flask import Flask, app
+from flask import Flask, app, request
 from flask_restful import Api, Resource
 from flaskapp import routes
 
 app = Flask(__name__)
+
+@app.route('/query', methods=['GET', 'POST'])
+def query():
+    if request.method == 'POST':
+        return 'POST received', 204
+    return 'GET received', 200
+
+
+@app.route('/documents')
+def documents():
+    return 'Hello, World!'
+
+
 api = Api(app)
 
 class HelloWorld(Resource):
