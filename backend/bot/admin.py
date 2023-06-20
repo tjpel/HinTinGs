37bacc948d77aae7eea0c4791ca6319856e89273
Travis Smalling <main@smalling.dev>
2023-06-19 21:20:03 -0500

    initial commit
    [WIP]
    framework being built for the flask-restful api to handle the backend, once I clean everything up should go much smoother

--- backend/bot/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Question, SourceDocument
-
-# Register your models here.
-admin.site.register(Question)
-admin.site.register(SourceDocument)
---
+++ backend/flaskapp/app.py
@@ -0,0 +1,4 @@
+from flask import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
